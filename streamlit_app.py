c43a6c5135dffda318f9e4d01818c1f3d4e3e0a1
Christian Avery <christianavery@christians-mbp.home>
2024-07-27 15:09:51 +0100

    updated requirements

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import bs4
 import getpass
 import os
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-import Chroma
+from langchain_chroma import Chroma
 #from langchain_openai import ChatOpenAI
 from langchain_openai import OpenAIEmbeddings
 
