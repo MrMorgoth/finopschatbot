955659f656d94bdc9dc384e7e8193fd9c1d8b811
Christian Avery <christianavery@christians-mbp.home>
2024-08-25 17:32:14 +0100

    Added generate_response function, removed unused text and added file uploader)

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,28 @@ from langchain_openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 
+# Initiate OpenAI client
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
+def generate_response(uploaded_file, openai_api_key, query_text):
+    # Load document if file is uploaded
+    if uploaded_file is not None:
+        documents = [uploaded_file.read().decode()]
+        # Split documents into chunks
+        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        texts = text_splitter.create_documents(documents)
+        # Select embeddings
+        embeddings = OpenAIEmbeddings(openai_api_key=st.secrets["OPENAI_API_KEY"])
+        # Create a vectorstore from documents
+        db = Chroma.from_documents(texts, embeddings)
+        # Create retriever interface
+        retriever = db.as_retriever()
+        # Create QA chain
+        qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
+        return qa.run(query_text)
+    
+# File upload
+uploaded_file = st.file_uploader('Upload an article', type='txt')
 
 
 # Show title and description.
@@ -15,16 +36,6 @@ st.title("💬 FinOps Chatbot")
 st.write(
     "This is a simple chatbot that uses Generative AI combined with FinOps specific content retrieved to generate more accurate responses. "
 )
-"""
-with st.sidebar:
-    st.button("Cost Overview")
-    st.button("Kubernetes Cost")
-    st.button("Cost Avoidance")
-    st.button("Rate Reduction")
-    st.button("Leaderboard")
-"""
-# Initiate OpenAI client
-client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 # Create a session state variable to store the chat messages. This ensures that the
 # messages persist across reruns.
