79810bdb7be6957ac34fcfe20338a89a5ec20c25
Christian Avery <christianavery@christians-mbp.home>
2024-08-25 17:54:33 +0100

    removed chromadb embeddings

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
+from langchain_community.vectorstores import FAISS
 
 # Initiate OpenAI client
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
@@ -20,9 +21,10 @@ def generate_response(uploaded_file, openai_api_key, query_text):
         # Select embeddings
         embeddings = OpenAIEmbeddings(openai_api_key=st.secrets["OPENAI_API_KEY"])
         # Create a vectorstore from documents
-        db = Chroma.from_documents(texts, embeddings)
+        vectorstore = FAISS.from_documents(documents=texts,
+                                   embedding=OpenAIEmbeddings())
         # Create retriever interface
-        retriever = db.as_retriever()
+        vectorstore.as_retriever()
         # Create QA chain
         qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
         return qa.run(query_text)
@@ -37,7 +39,7 @@ st.write(
 )
 
 # File upload
-uploaded_file = st.file_uploader('Upload an article', type='txt')
+uploaded_file = st.file_uploader('Upload a file', type='txt')
 # Query text
 query_text = st.text_input('Enter your question:', placeholder = 'Please provide a short summary.', disabled=not uploaded_file)
 
