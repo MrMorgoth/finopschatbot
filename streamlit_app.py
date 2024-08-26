07a421a48446e60aca166dd5800a2f89798416e3
Christian Avery <christianavery@christians-mbp.home>
2024-08-26 13:19:43 +0100

    attempt to fix type error

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ def generate_response(uploaded_file, openai_api_key, query_text):
         # Create retriever interface
         retriever = vectorstore.as_retriever()
         # Create QA chain
-        qa = RetrievalQA.from_chain_type(llm=client, chain_type='stuff', retriever=retriever)
+        qa = RetrievalQA.from_chain_type(llm=OpenAI(api_key=st.secrets["OPENAI_API_KEY"]), chain_type='stuff', retriever=retriever)
         return qa.run(query_text)
     
 
