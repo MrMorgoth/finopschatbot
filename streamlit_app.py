644c637c7bc6d41af9332a846f54101eea713f78
Christian Avery <christianavery@christians-mbp.home>
2024-08-27 00:27:46 +0100

    using glob to find and print local text files

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,11 +30,10 @@ for file in search:
 
 
 def list_response(query_text):
-    documents = []
-    for doc in txt_files:
-        st.info(doc)
-        #output = doc.read().decode()
-        #documents.append(output)
+    documents = ["context/personas.txt".read().decode()]
+    #for doc in txt_files:
+    #    output = doc.read().decode()
+    #    documents.append(output)
     # Split documents into chunks
     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
     texts = text_splitter.create_documents(documents)
