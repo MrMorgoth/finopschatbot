f27cd85eaa3d084ffde66a30e0c2e4534a34a82b
Christian Avery <christianavery@christians-mbp.home>
2024-09-02 12:08:40 +0100

    added write statement for response to test where the optimal reservation value is being lost

--- pages/rate_reduction.py
+++ pages/rate_reduction.py
@@ -46,8 +46,8 @@ with st.form('myform', clear_on_submit=True):
     submitted = st.form_submit_button('Submit', disabled=not(uploaded_file, percentage_discount_rate))
     if submitted:
         percentage_discount_rate = int(percentage_discount_rate)
+        st.write(percentage_discount_rate)
         discount_rate = percentage_discount_rate // 100
-        st.write(discount_rate)
         response = calculate_optimal_reservation(data, discount_rate)
         result.append(response)
 
