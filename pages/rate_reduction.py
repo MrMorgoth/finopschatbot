54c70e04229ca4af14972857ed3ddfa635ede748
Christian Avery <christianavery@christians-mbp.home>
2024-09-02 12:16:25 +0100

    Working on fixing the bar plot function

--- pages/rate_reduction.py
+++ pages/rate_reduction.py
@@ -51,7 +51,7 @@ with st.form('myform', clear_on_submit=True):
         result.append(response)
 
 if len(result):
-    st.bar_chart(data=data)
+    #st.bar_chart(data=data)
     with st.chat_message("user"):
         st.write("Hello 👋")
         st.write("The average On-Demand usage for this period is $", avg_ondemand_cost, "/hour")
