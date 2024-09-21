127c98c331ac6a4cbb4bccc1629571d74660e128
Christian Avery <christianavery@Christians-MacBook-Pro.local>
2024-09-21 20:25:04 +0100

    new tenant_review page

---
+++ pages/tenant_review.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import boto3
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime, timedeltafrom 
+botocore.exceptions import NoCredentialsError, PartialCredentialsError
+from streamlit_extras.metric_cards import style_metric_cards
+
+st.write("New Page!")
