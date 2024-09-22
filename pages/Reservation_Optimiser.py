1df9dc2ce2720a3e98c320f11fcdf43ca4d412fa
Christian Avery <christianavery@Christians-MacBook-Pro.local>
2024-09-22 18:02:14 +0100

    added top instances to container

--- pages/Reservation_Optimiser.py
+++ pages/Reservation_Optimiser.py
@@ -118,7 +118,10 @@ if aws_access_key_id and aws_secret_access_key:
     top_5_instances, message = get_top_rds_ec2_costs(aws_access_key_id, aws_secret_access_key, region_name)
     if top_5_instances is not None:
         # Display the top 5 instances
-        st.write(top_5_instances)
+        for instance in top_5_instances:
+            tile = instance.container(height=120)
+            tile.title(":balloon:")
+#        st.write(top_5_instances)
 
             # Plot the top 5 instances with Matplotlib
             #plt.bar(top_5_instances['Instance Type'], top_5_instances['Cost'], color='skyblue')
